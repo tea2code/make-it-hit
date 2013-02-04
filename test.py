5cd197d1567f7dcfb9e3798b39e45643bc968f93
tea2code <stefan4875@gmail.com>
2013-02-04 18:16:53 +0100

    Renamed doctest.py -> test.py.

---
+++ test.py
@@ -0,0 +1,9 @@
+import common.vector2d
+import data.data
+import data.movable
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod( common.vector2d )
+    doctest.testmod( data.data )
+    doctest.testmod( data.movable )
