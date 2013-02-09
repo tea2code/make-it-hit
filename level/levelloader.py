daa77d3fead47c5cccc0c986aaf545b30ae67aba
tea2code <stefan4875@gmail.com>
2013-02-09 13:56:18 +0100

    Added level loader. Closes #8.

---
+++ level/levelloader.py
@@ -0,0 +1,11 @@
+﻿from . import levelparser
+
+class LevelLoader():
+    ''' Loads levels from files. '''
+    
+    def load( self, mapFile ):
+        ''' Loads the given level file. Throws xml.etree.ElementTree.ParseError or level.levelparsererror
+        in case of an error. Returns the level object. '''
+        parser = levelparser.LevelParser()
+        return parser.parse( mapFile )
+        
